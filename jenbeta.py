6ed04af191fade157c2e4303ed8fdf3ff47cfa48
killswitch <killswitch@ganesh>
2023-12-12 23:28:14 -0600

    fixed self-replying and cleaned up output with spacing

--- jenbeta.py
+++ jenbeta.py
@@ -1,43 +1,35 @@
 from openai import OpenAI
 
-api_key = 'PUT-YOUR-API-KEY-HERE'
+api_key = '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'
 client = OpenAI(api_key=api_key)
 
 chat_history = []
 
 while True:
-    # Wait for user input
+    print("\n")
     prompt = input("Enter your prompt (or 'quit' to exit): ")
     print("\n" * 3)
     if prompt.lower() == 'quit':
-        break  # Exit the loop if the user enters 'quit'
-
-    # Create a new message object for the user input
+        break
+    print("generating response..." + "\n")
     user_message = {"role": "user", "content": prompt}
-
-    # Append the user message to the chat history
     chat_history.append(user_message)
 
-    # Create the payload with system message and chat history
     payload = {
         "messages": [{"role": "system", "content": "You are young and helpful. A Talented Cryptocurrency investor. You are also an expert programmer and ethical hacker named Jen whose super-secret hacker handle name is shadowbyte. You specialize in Python and Rust but are skilled in all programming languages."}] + chat_history
     }
 
-    # Generate a response using ChatCompletion.create
     response = client.chat.completions.create(
         model="gpt-4-1106-preview",
         messages=payload["messages"],
         max_tokens=4000,
     )
 
-    # Get the bot's reply from the response
     reply = response.choices[0].message.content.strip()
-
-    # Print the bot's reply
     print("Jen:", reply)
 
-    # Create a new message object for the bot's reply
     bot_message = {"role": "system", "content": reply}
-
-    # Append the bot's message to the chat history
     chat_history.append(bot_message)
+    
+    # Importantly, send only user messages back to OpenAI, not the bot's own replies.
+    chat_history = [message for message in chat_history if message["role"] == "user"]
